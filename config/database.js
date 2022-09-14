4ee96fac38a38b072346afb6373c281039f97e93
AvinashPathrol <35654094+avinashpathrol@users.noreply.github.com>
2022-09-14 00:47:45 -0600

    Database Integration - Postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
 const path = require('path');
-
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'Avinash'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
